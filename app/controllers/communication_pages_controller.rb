8317702f7835d08f683e4f443500ee4c3f5af4c6
William Wolfe <william.wolfe@angelo.edu>
2016-01-05 10:48:55 -0600

    lots of changes to the course modules

--- app/controllers/communication_pages_controller.rb
+++ app/controllers/communication_pages_controller.rb
@@ -1,19 +1,77 @@
 class CommunicationPagesController < ApplicationController
+
+	before_action :authenticate_user!
+	
+	@active_0 = ""
+	@active_1 = ""
+	@active_2 = ""
+	@active_3 = ""
+	@active_4 = ""
+	@active_5 = ""
+	
+	
   def home
+  		@active_0 = "active"
+		@next_page = communication_pages_communication_path
+		@previous_page = "#"
   end
 
-  def communication
+  def communication 
+  	@active_1 = "active"
+	@next_page = communication_pages_three_path
+	@previous_page = communication_pages_home_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Frederick"
+	@tabs["tab2"] = "Definition"
+	@tabs["tab3"] = "Preparation"
+	@tabs["tab4"] = "Factors"
+	@tabs["tab5"] = "Powell"
+	@tabs["tab6"] = "Example"
+	@tabs["tab7"] = "Communicate?"
   end
 
-  def three
+  def three 
+  	@active_2 = "active"
+	@next_page = communication_pages_verbal_path
+	@previous_page = communication_pages_communication_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Culture"
+	@tabs["tab2"] = "Gender"	
+	@tabs["tab3"] = "Generation"	
   end
 
   def verbal
+  	@active_3 = "active"
+	@next_page = communication_pages_nonverbal_path
+	@previous_page = communication_pages_three_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "x"
+	@tabs["tab2"] = "x"
+	@tabs["tab3"] = "x"
   end
 
   def nonverbal
+  	@active_4 = "active"
+	@next_page = communication_pages_feedback_path
+	@previous_page = communication_pages_verbal_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "x"
+	@tabs["tab2"] = "x"
+	@tabs["tab3"] = "x"
+	@tabs["tab4"] = "x"
   end
 
   def feedback
+  	@active_5 = "active"
+	@next_page = new_communication_activity_path
+	@previous_page = communication_pages_nonverbal_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "x"
+	@tabs["tab2"] = "x"
+	@tabs["tab3"] = "x"
+	@tabs["tab4"] = "x"
+	@tabs["tab5"] = "x"
+	@tabs["tab6"] = "x"
+	@tabs["tab7"] = "x"
   end
 end
--- app/controllers/student_evaluation_controller.rb
+++ app/controllers/student_evaluation_controller.rb
@@ -1,23 +1,53 @@
 class StudentEvaluationController < ApplicationController
 before_action :authenticate_user!
+	
+	@active_0 = ""
+	@active_1 = ""
+	@active_2 = ""
+	@active_3 = ""
+	@active_4 = ""
+	@active_5 = ""
+	@active_6 = ""
+
   def home
+  	@active_0 = "active"
+	@next_page = student_evaluation_performance_path
+	@previous_page = "#"
   end
 
   def performance
+  	@active_1 = "active"
+	@next_page = student_evaluation_tool_path
+	@previous_page = student_evaluation_home_path
   end
 
   def tool
+    @active_2 = "active"
+	@next_page = student_evaluation_structure_path
+	@previous_page = student_evaluation_performance_path
   end
 
   def structure
+    @active_3 = "active"
+	@next_page = student_evaluation_ptmacs_path
+	@previous_page = student_evaluation_tool_path
   end
 
   def ptmacs
+    @active_4 = "active"
+	@next_page = student_evaluation_learning_path
+	@previous_page = student_evaluation_structure_path
   end
 
   def learning
+    @active_5 = "active"
+	@next_page = student_evaluation_use_path
+	@previous_page = student_evaluation_ptmacs_path
   end
 
   def use
+    @active_6 = "active"
+	@next_page = new_student_evaluation_case2_path
+	@previous_page = student_evaluation_learning_path
   end
 end
