e1eb95bbaff9908964a3e70a3bd93fd70979e30c
William Wolfe <william.wolfe@angelo.edu>
2016-01-08 14:52:12 -0600

    working on the teaching and learning section - then went back to the student evaluation module

--- app/controllers/student_evaluation_controller.rb
+++ app/controllers/student_evaluation_controller.rb
@@ -19,35 +19,63 @@ before_action :authenticate_user!
   	@active_1 = "active"
 	@next_page = student_evaluation_tool_path
 	@previous_page = student_evaluation_home_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Performance"
   end
 
   def tool
     @active_2 = "active"
 	@next_page = student_evaluation_structure_path
 	@previous_page = student_evaluation_performance_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Philosophy"
+	@tabs["tab2"] = "Features"
+	@tabs["tab3"] = "Standards"
   end
 
   def structure
     @active_3 = "active"
 	@next_page = student_evaluation_ptmacs_path
 	@previous_page = student_evaluation_tool_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Structure"
+	@tabs["tab2"] = "Template"
   end
 
   def ptmacs
     @active_4 = "active"
 	@next_page = student_evaluation_learning_path
 	@previous_page = student_evaluation_structure_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Content"
+	@tabs["tab2"] = "Skills"
+	@tabs["tab3"] = "Sheets"
+	@tabs["tab4"] = "Forms"
   end
 
   def learning
     @active_5 = "active"
 	@next_page = student_evaluation_use_path
 	@previous_page = student_evaluation_ptmacs_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Grading"
+	@tabs["tab2"] = "Student_Training"
+	@tabs["tab3"] = "CI_Training"
+	@tabs["tab4"] = "Planning"
+	@tabs["tab5"] = "Responsibility"
   end
 
   def use
     @active_6 = "active"
 	@next_page = new_student_evaluation_case2_path
 	@previous_page = student_evaluation_learning_path
+	@tabs = Hash.new
+	@tabs["tab1"] = "Document"
+	@tabs["tab2"] = "Rate"
+	@tabs["tab3"] = "Approve"
+	@tabs["tab4"] = "Comment"
+	@tabs["tab5"] = "Report"
+	@tabs["tab6"] = "Evaluate"
+	@tabs["tab7"] = "Required"
   end
 end
