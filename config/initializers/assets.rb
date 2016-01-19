a0e2e11e28249a76ea7a6ae65ee97b0d6810f2fd
William Wolfe <william.wolfe@angelo.edu>
2016-01-19 16:38:47 -0600

    changed a color

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-#Rails.application.config.assets.precompile += %w( creative/manifest.js creative/manifest.css )
+#Not sure if I need this line:
+Rails.application.config.assets.precompile += %w( creative/manifest.js creative/manifest.css )
